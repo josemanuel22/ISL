596bc6ba176615ed54c368f56f8148b952ed033a
josemanuel22 <josemanuel.defrutos22@gmail.com>
2023-06-23 15:09:50 +0200

    Adding documentation

--- examples/JointDistributionLearning.jl
+++ examples/JointDistributionLearning.jl
@@ -7,9 +7,9 @@ model = Chain(
 line(x; m, b) = m * x + b
 model1(x) =  line(x; m=3, b=5)
 model2(x) =  line(x; m=-2, b=2)
-
 realModel(x, ϵ) =  ϵ < 0.1 ? model1(x) : model2(x)
 
+#Learning with custom loss
 η = 0.1; num_epochs = 100; n_samples = 1000
 optim = Flux.setup(Flux.Adam(η), model)
 losses = []
@@ -28,5 +28,26 @@ l = CustomLoss(2)
     end
     Flux.update!(optim, model, grads[1])
     push!(losses, loss)
-end
+end;
 
+#Learning with classical mse loss
+η = 0.1; num_epochs = 1000;
+optim = Flux.setup(Flux.Adam(η), model)
+losses_mse = []
+for epoch in 1:num_epochs
+    loss, grads = Flux.withgradient(model) do m
+        x = rand(Normal(μ, stddev), l.K)
+        yₖ = m(x')
+        y = realModel(rand(Normal(μ, stddev)), rand(Float64))
+        Flux.mse(yₖ, y)
+    end
+    Flux.update!(optim, model, grads[1])
+    push!(losses_mse, loss)
+end;
+
+x = rand(Normal(μ, stddev), 1000)
+ϵ = rand(Float64, 1000)
+y = realModel.(x, ϵ)
+ŷ = model(x')
+Plots.plot(x, y, seriestype = :scatter)
+Plots.plot!(x, ŷ', seriestype = :scatter)
--- src/CustomLossFunction.jl
+++ src/CustomLossFunction.jl
@@ -8,24 +8,44 @@ end;
 scalar_diff = (loss::CustomLoss, a_k) -> sum((a_k .- (1 ./ (loss.K + 1))) .^2)
 jensen_shannon_∇ = (loss::CustomLoss, a_k) -> jensen_shannon_divergence(a_k, fill(1 / (loss.K + 1), 1, loss.K + 1))
 
-function jensen_shannon_divergence(p,q)
-    ϵ = 1e-3
+function jensen_shannon_divergence(p, q)
+    ϵ = 1e-3 # to avoid log(0)
     return 0.5 * (kldivergence(p.+ϵ, q.+ϵ) + kldivergence(q.+ϵ, p.+ϵ))
 end;
 
+"""
+    sigmoid(ŷ, y)
+
+    Sigmoid function centered at y.
+"""
 function sigmoid(ŷ, y)
     return sigmoid_fast.((ŷ-y)*10)
 end;
 
+"""
+    ψₘ(y, m)
+
+    Bump function centered at m. Implemented as a gaussian function.
+"""
 function ψₘ(y, m)
     stddev = 0.1
     return exp.((-0.5 .* ((y .- m) ./ stddev) .^ 2))
 end
 
+"""
+    ϕ(yₖ, yₙ)
+
+    Sum of the sigmoid function centered at yₙ applied to the vector yₖ.
+"""
 function ϕ(yₖ, yₙ)
     return sum(sigmoid.(yₙ, yₖ))
 end;
 
+"""
+    γ(yₖ, yₙ, m, K)
+    
+    Calculate the contribution of ψₘ ∘ ϕ(yₖ, yₙ) to the m bin of the histogram (Vector{Float}).
+"""
 function γ(yₖ, yₙ, m, K)
     eₘ(m) = [j == m ? 1.0 : 0.0 for j in 0:K-1]
     return eₘ(m) * ψₘ(ϕ(yₖ, yₙ), m)
@@ -36,11 +56,17 @@ end;
 
 Apply the γ function to the given parameters. 
 This function is faster than the original γ function because it uses StaticArrays.
-However because Zigote does not support StaticArrays, this function can not be used in the training process.
+However because Zygote does not support StaticArrays, this function can not be used in the training process.
 """
 function γ_fast(yₖ, yₙ, m, K)
     eₘ(m) = SVector{K, Float64}(j == m ? 1.0 : 0.0 for j in 0:K-1)
     return eₘ(m) * ψₘ(ϕ(yₖ, yₙ), m)
 end;
 
+"""
+    generate_aₖ(loss, ŷ, y)
+
+    Generate a one step histogram (Vector{Float}) of the given vector ŷ of K simulted observations and the real data y.
+    generate_aₖ(loss, ŷ, y) = ∑ₖ γ(ŷ, y, k)
+"""
 generate_aₖ(loss, ŷ, y) = sum([γ(ŷ, y, k, loss.K+1) for k in 0:loss.K])
