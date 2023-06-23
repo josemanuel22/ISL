ff05bc42fb4d9cff60df27e183977b12ca2be8f4
josemanuel22 <josemanuel.defrutos22@gmail.com>
2023-06-23 15:46:59 +0200

    remove unnecessary params

--- src/CustomLossFunction.jl
+++ src/CustomLossFunction.jl
@@ -1,12 +1,6 @@
-struct CustomLoss
-    K::Int
-    function CustomLoss(K::Int)
-        new(K)
-    end;
-end;
 
-scalar_diff = (loss::CustomLoss, a_k) -> sum((a_k .- (1 ./ (loss.K + 1))) .^2)
-jensen_shannon_∇ = (loss::CustomLoss, a_k) -> jensen_shannon_divergence(a_k, fill(1 / (loss.K + 1), 1, loss.K + 1))
+scalar_diff(aₖ) = sum((aₖ .- (1 ./ length(aₖ))) .^2)
+jensen_shannon_∇(aₖ) = jensen_shannon_divergence(aₖ, fill(1 / length(aₖ), 1, length(aₖ)))
 
 function jensen_shannon_divergence(p, q)
     ϵ = 1e-3 # to avoid log(0)
@@ -42,31 +36,31 @@ function ϕ(yₖ, yₙ)
 end;
 
 """
-    γ(yₖ, yₙ, m, K)
+    γ(yₖ, yₙ, m)
     
     Calculate the contribution of ψₘ ∘ ϕ(yₖ, yₙ) to the m bin of the histogram (Vector{Float}).
 """
-function γ(yₖ, yₙ, m, K)
-    eₘ(m) = [j == m ? 1.0 : 0.0 for j in 0:K-1]
+function γ(yₖ, yₙ, m)
+    eₘ(m) = [j == m ? 1.0 : 0.0 for j in 0:length(yₖ)]
     return eₘ(m) * ψₘ(ϕ(yₖ, yₙ), m)
 end;
 
 """
-    γ_fast(yₖ, yₙ, m, K)
+    γ_fast(yₖ, yₙ, m)
 
 Apply the γ function to the given parameters. 
 This function is faster than the original γ function because it uses StaticArrays.
 However because Zygote does not support StaticArrays, this function can not be used in the training process.
 """
 function γ_fast(yₖ, yₙ, m, K)
-    eₘ(m) = SVector{K, Float64}(j == m ? 1.0 : 0.0 for j in 0:K-1)
+    eₘ(m) = SVector{K, Float64}(j == m ? 1.0 : 0.0 for j in 0:length(yₖ))
     return eₘ(m) * ψₘ(ϕ(yₖ, yₙ), m)
 end;
 
 """
-    generate_aₖ(loss, ŷ, y)
+    generate_aₖ(ŷ, y)
 
     Generate a one step histogram (Vector{Float}) of the given vector ŷ of K simulted observations and the real data y.
-    generate_aₖ(loss, ŷ, y) = ∑ₖ γ(ŷ, y, k)
+    generate_aₖ(ŷ, y) = ∑ₖ γ(ŷ, y, k)
 """
-generate_aₖ(loss, ŷ, y) = sum([γ(ŷ, y, k, loss.K+1) for k in 0:loss.K])
+generate_aₖ(ŷ, y) = sum([γ(ŷ, y, k) for k in 0:length(ŷ)])
