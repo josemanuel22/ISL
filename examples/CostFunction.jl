a30fca4956ef7c243c5a8ed9c09e7e3fa944d7fd
josemanuel22 <josemanuel.defrutos22@gmail.com>
2023-07-03 19:51:13 +0200

    proxy function paralelization

--- examples/CostFunction.jl
+++ examples/CostFunction.jl
@@ -8,11 +8,11 @@ function proxi_cost_function(mesh, model::Function, target::Function, K::Int, n_
         for bᵢ in bs
             loss = 0.
             aₖ = zeros(K+1)
-            for _ in 1:n_samples
+            aₖ = ThreadsX.sum(1:n_samples) do _
                 x = rand(Normal(μ, stddev), K)
                 yₖ = model.(x', m=mᵢ, b=bᵢ)
                 y = target(rand(Normal(μ, stddev)))
-                aₖ += generate_aₖ(yₖ, y)
+                generate_aₖ(yₖ, y)
             end
             loss = scalar_diff(aₖ ./ sum(aₖ))
             push!(losses, loss)
@@ -24,7 +24,7 @@ end;
 function real_cost_function(mesh, model::Function, target::Function, K::Int, n_samples::Int)
     losses::Vector{Float64} = []
     ms, bs = mesh
-    Threads.@threads for mᵢ in ms
+    for mᵢ in ms
         for bᵢ in bs
             m(x) = model(x; m=mᵢ, b=bᵢ) 
             windows = get_window_of_Aₖ(m, target, K, n_samples)
@@ -45,21 +45,21 @@ truthh(x) =  model1(x; m=m, b=b)
 μ = 0
 stddev = 1
 
-res=100; n_samples = 1000
+res=100; n_samples = 1000; K = 2
 losses = []
-ms = LinRange(m-10, m+10, res)
-bs = LinRange(b-10, b+10, res)
+ms = LinRange(m-5, m+5, res)
+bs = LinRange(b-5, b+5, res)
 @showprogress for mᵢ in ms
     for bᵢ in bs
         loss = 0.
         aₖ = zeros(K+1)
-        for _ in 1:n_samples
+        aₖ = ThreadsX.sum(1:n_samples) do _
             x = rand(Normal(μ, stddev), K)
             yₖ = model1.(x', m=mᵢ, b=bᵢ)
             y = truthh(rand(Normal(μ, stddev)))
-            aₖ += generate_aₖ(yₖ, y)
+            generate_aₖ(yₖ, y)
         end
-        loss = scalar_diff(aₖ ./ sum(aₖ))
+        loss = jensen_shannon_∇(aₖ ./ sum(aₖ))
         push!(losses, loss)
     end
 end
@@ -67,15 +67,16 @@ end
 for mᵢ in ms
     for bᵢ in bs
         model2(x) =  model1(x; m=mᵢ, b=bᵢ)
-        windows = get_window_of_Aₖ(model2, truthh, K, n_samples)
+        windows = get_window_of_Aₖ(model1, truthh, K, n_samples)
         aₖ = [count(x -> x == i, windows) for i in 0:K]
         loss = scalar_diff(aₖ ./ sum(aₖ))
+        scalar_diff(aₖ ./ sum(aₖ))
         push!(losses, loss)
     end
 end
 
 
-plot(ms, losses)
+plot(bs, losses)
 Plots.plot(ms,bs,reshape(losses, (res,res)),st=:surface, title=string("N=",n_samples," K=", K," res=",res))
 xlabel!("m")
 ylabel!("b")
