507a1ec27449609735a5013f383b32fc8c2c945b
josemanuel22 <josemanuel.defrutos22@gmail.com>
2023-07-22 08:42:43 +0200

    style blue

--- src/AdaptativeBlockTest.jl
+++ src/AdaptativeBlockTest.jl
@@ -3,9 +3,12 @@
 
     Generate a window of the rv's Aₖ for a given model and target function.
 """
-function get_window_of_Aₖ(model, target , K, n_samples)
-    μ = 0; stddev = 1;
-    count.([model(rand(Normal(μ, stddev), K)') .< target(rand()) for _ in 1:n_samples])
+function get_window_of_Aₖ(model, target, K, n_samples)
+    μ = 0
+    stddev = 1
+    return count.([
+        model(rand(Normal(μ, stddev), K)') .< target(rand()) for _ in 1:n_samples
+    ])
 end;
 
 """
@@ -15,5 +18,5 @@ end;
     It is implemented using a Chi-Square test.
 """
 function convergence_to_uniform(aₖ)
-    return pvalue(ChisqTest(aₖ, fill(1/length(aₖ), length(aₖ))))
+    return pvalue(ChisqTest(aₖ, fill(1 / length(aₖ), length(aₖ))))
 end;
