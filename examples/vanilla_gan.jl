3bf91fe223fa67bf8aee0ff0da6514b828759661
josemanuel22 <josemanuel.defrutos22@gmail.com>
2023-07-28 11:50:20 +0200

    putting nice

--- examples/vanilla_gan.jl
+++ examples/vanilla_gan.jl
@@ -11,9 +11,9 @@ using Printf
 using CUDA
 using Zygote
 
-if has_cuda()		# Check if CUDA is available
+if has_cuda()# Check if CUDA is available
     @info "CUDA is on"
-    import CuArrays		# If CUDA is available, import CuArrays
+    using CuArrays: CuArrays# If CUDA is available, import CuArrays
     CuArrays.allowscalar(false)
 end
 
@@ -21,54 +21,41 @@ end
     data_size::Int = 10000
     batch_size::Int = 100
     latent_dim::Int = 1
-    epochs::Int = 10000
+    epochs::Int = 20
     verbose_freq::Int = 1000
-    output_x::Int = 6        # No. of sample images to concatenate along x-axis
-    output_y::Int = 6        # No. of sample images to concatenate along y-axis
-    lr_dscr::Float64 = 0.00002
-    lr_gen::Float64 = 0.00002
+    lr_dscr::Float64 = 0.000001
+    lr_gen::Float64 = 0.000002
+
+    dscr_steps::Int = 5
+    gen_steps::Int = 1
+end
+
+function real_model(ϵ)
+    return rand(LogNormal(3,1))
 end
 
 function generator(args)
-    return Chain(Dense(1, 20, tanh), Dense(20, 1)) |> gpu
+    return gpu(Chain(Dense(1, 10, tanh), Dense(10, 10, tanh), Dense(10, 1)))
 end
 
 function discriminator(args)
-    return Chain(Dense(1, 20), x -> leakyrelu.(x,0.2f0), Dense(20, 1, σ)) |> gpu
+    return gpu(Chain(Dense(100, 200, tanh), Dense(200, 200, tanh), Dense(200, 1, σ)))
 end
 
-function load_data(hparams)
-    function realModel(ϵ)
-        μ₁=-20; σ₁ = 2;
-        μ₂=0; σ₂ = 1;
-        μ₃=40; σ₃ = 2;
-        if ϵ < 0.3
-            return rand(Normal(μ₁, σ₁))
-        elseif ϵ < 0.7
-            return rand(Normal(μ₂, σ₂))
-        else
-            return rand(Normal(μ₃, σ₃))
-    end
-
-    data = collect(partition(realModel.(rand(Float64, hparams.data_size)), hparams.batch_size))
-    return data
-end
-
-# Loss functions
 function discr_loss(real_output, fake_output)
-    real_loss = mean(binarycrossentropy.(real_output, 1f0))
-    fake_loss = mean(binarycrossentropy.(fake_output, 0f0))
+    real_loss = mean(binarycrossentropy.(real_output, 1.0f0))
+    fake_loss = mean(binarycrossentropy.(fake_output, 0.0f0))
     return (real_loss + fake_loss)
 end
 
-generator_loss(fake_output) = mean(binarycrossentropy.(fake_output, 1f0))
+generator_loss(fake_output) = mean(binarycrossentropy.(fake_output, 1.0f0))
 
 function train_discr(discr, original_data, fake_data, opt_discr)
     ps = Flux.params(discr)
     loss, back = Zygote.pullback(ps) do
-                      discr_loss(discr(original_data'), discr(fake_data))
+        discr_loss(discr(original_data), discr(fake_data'))
     end
-    grads = back(1f0)
+    grads = back(1.0f0)
     update!(opt_discr, ps, grads)
     return loss
 end
@@ -76,15 +63,33 @@ end
 Zygote.@nograd train_discr
 
 function train_gan(gen, discr, original_data, opt_gen, opt_discr, hparams)
-    noise = randn!(similar(original_data, (hparams.batch_size, hparams.latent_dim))) |> gpu
+    noise = gpu(randn!(similar(original_data, (hparams.batch_size, hparams.latent_dim))))
     loss = Dict()
     ps = Flux.params(gen)
-    loss["gen"], back = Zygote.pullback(ps) do
-                          fake_ = gen(noise')
-                          loss["discr"] = train_discr(discr, original_data, fake_, opt_discr)
-                          generator_loss(discr(fake_))
+    for _ in 1:hparams.dscr_steps
+        loss["gen"], back = Zygote.pullback(ps) do
+            fake_ = gen(noise')
+            generator_loss(discr(fake_'))
+        end
     end
-    grads = back(1f0)
+    grads = back(1.0f0)
+    update!(opt_gen, ps, grads)
+
+    for _ in 1:hparams.gen_steps
+        Zygote.pullback(ps) do
+            fake_ = gen(noise')
+            loss["discr"] = train_discr(discr, original_data, fake_, opt_discr)
+        end
+    end
+    grads = back(1.0f0)
+    update!(opt_gen, ps, grads)
+
+    loss["gen"], back = Zygote.pullback(ps) do
+        fake_ = gen(noise')
+        loss["discr"] = train_discr(discr, original_data, fake_, opt_discr)
+        generator_loss(discr(fake_'))
+    end
+    grads = back(1.0f0)
     update!(opt_gen, ps, grads)
     return loss
 end
@@ -92,15 +97,15 @@ end
 function train()
     hparams = HyperParams()
 
-    data = load_data(hparams)
+    train_set = real_model.(rand(Float64, hparams.data_size))
+    loader = gpu(Flux.DataLoader(
+        train_set; batchsize=hparams.batch_size, shuffle=true, partial=false
+    ))
 
-    # Discriminator
     dscr = discriminator(hparams)
 
-    # Generator
-    gen =  generator(hparams) |> gpu
+    gen = gpu(generator(hparams))
 
-    # Optimizers
     opt_dscr = ADAM(hparams.lr_dscr)
     opt_gen = ADAM(hparams.lr_gen)
 
@@ -108,24 +113,13 @@ function train()
     losses_gen = []
     losses_dscr = []
     train_steps = 0
-    for ep in 1:hparams.epochs
-        @info "Epoch $ep"
-        for x in data
+    for ep in 1:(hparams.epochs)
+        if train_steps % hparams.verbose_freq == 0
+            @info "Epoch $ep"
+        end
+        for x in loader
             # Update discriminator and generator
             loss = train_gan(gen, dscr, x, opt_gen, opt_dscr, hparams)
-
-            #if train_steps % hparams.verbose_freq == 0
-            #    @info("Train step $(train_steps), Discriminator loss = $(loss["discr"]), Generator loss = $(loss["gen"])")
-                # Save generated fake image
-            #    x = rand(Normal(μ, stddev), 100000)
-            #    ϵ = rand(Float64, 100000)
-            #    y = realModel.(ϵ)
-            #    histogram(y, bins=-50:1:70, label = "distrubución real", title = "proxy error cuadrático")
-            #    ŷ = gen(x')
-            #    histogram!(ŷ', bins=-50:1:70, label = "distrubución aproximada")
-            #    #output_image = create_output_image(gen, fixed_noise, hparams)
-            #    #save(@sprintf("output/gan_steps_%06d.png", train_steps), output_image)
-            #end
             push!(losses_gen, loss["gen"])
             push!(losses_dscr, loss["discr"])
             train_steps += 1
@@ -133,9 +127,12 @@ function train()
     end
 end
 
-x = rand(Normal(μ, stddev), 100000)
-ϵ = rand(Float64, 100000)
-y = realModel.(ϵ)
-histogram(y, bins=-50:1:70, label = "distrubución real", title = "proxy error cuadrático")
-ŷ = gen(x')
-histogram!(ŷ', bins=-50:1:70, label = "distrubución aproximada")
+function plot_model(real_model, model, range)
+    μ = 0; stddev = 1
+    x = rand(Normal(μ, stddev), 1000000)
+    ϵ = rand(Float64, 1000000)
+    y = real_model.(ϵ)
+    histogram(y; bins=range)
+    ŷ = model(x')
+    histogram!(ŷ'; bins=range)
+end
--- examples/wgan.jl
+++ examples/wgan.jl
@@ -21,23 +21,24 @@ end
     data_size::Int = 10000
     batch_size::Int = 100
     latent_dim::Int = 1
-    epochs::Int = 30
+    epochs::Int = 100
     n_critic::Int = 5
     clip_value::Float32 = 0.01
-    verbose_freq::Int = 1000
-    output_x::Int = 6        # No. of sample images to concatenate along x-axis
-    output_y::Int = 6        # No. of sample images to concatenate along y-axis
-    lr_dscr::Float64 = 0.00005
-    lr_gen::Float64 = 0.00005
+    lr_dscr::Float64 =  0.00005
+    lr_gen::Float64 =   0.00005
+end
+
+function real_model(ϵ)
+    rand(Pareto(1, 2))
 end
 
 ## Generator and Discriminator
 function generator(args)
-    return Chain(Dense(1, 200, tanh), Dense(200, 200, tanh), Dense(200, 1)) |> gpu
+    return Chain(Dense(1, 5, tanh), Dense(5, 5, tanh), Dense(5, 1)) |> gpu
 end
 
 function discriminator(args)
-    return Chain(Dense(100, 2, tanh), Dense(2, 1, σ)) |> gpu
+    return Chain(Dense(100, 200, tanh), Dense(200, 500, tanh), Dense(500, 500, relu), Dense(500, 1, σ))  |> gpu
 end
 
 function wasserstein_loss_discr(real, fake)
@@ -83,53 +84,39 @@ end
 function train()
     hparams = HyperParams()
 
-    function realModel(ϵ)
-        μ₁=-5; σ₁ = 1;
-        μ₂=0; σ₂ = 0.3;
-        μ₃=2; σ₃ = 0.5;
-        if ϵ < 0.3
-            return rand(Normal(μ₁, σ₁))
-        elseif ϵ < 0.7
-            return rand(Normal(μ₂, σ₂))
-        else
-            return rand(Normal(μ₃, σ₃))
-        end
-    end
-
     train_set = realModel.(rand(Float64, hparams.data_size))
     loader = Flux.DataLoader(train_set, batchsize = hparams.batch_size, shuffle = true, partial=false) |> gpu
 
-    # Discriminator
     dscr = discriminator(hparams)
-
-    # Generator
     gen =  generator(hparams) |> gpu
 
-    # Optimizers
     opt_dscr = RMSProp(hparams.lr_dscr)
     opt_gen = RMSProp(hparams.lr_gen)
+    losses_gen = []
+    losses_dscr = []
 
     # Training
     train_steps = 0
     for ep in 1:hparams.epochs
-        @info "Epoch $ep"
+        if train_steps % hparams.verbose_freq == 0
+            @info "Epoch $ep"
+        end
         for x in loader
             # Update discriminator and generator
             loss = train_gan(gen, dscr, x, opt_gen, opt_dscr, hparams)
-
-            if train_steps % hparams.verbose_freq == 0
-                @info "Epoch $ep"
-            end
             train_steps += 1
-        push!(losses_gen, loss["gen"])
-        push!(losses_dscr, loss["discr"])
+            push!(losses_gen, loss["gen"])
+            push!(losses_dscr, loss["discr"])
         end
     end
 end
 
-x = rand(Normal(μ, stddev), 10000)
-ϵ = rand(Float64, 10000)
-y = realModel.(ϵ)
-histogram(y, bins=-10:0.1:5, label = "distrubución real", title = "proxy error cuadrático")
-ŷ = gen(x')
-histogram!(ŷ', bins=-10:0.1:5, label = "distrubución aproximada")
+function plot_model(real_model, model, range)
+    μ = 0; stddev = 1
+    x = rand(Normal(μ, stddev), 1000000)
+    ϵ = rand(Float64, 1000000)
+    y = real_model.(ϵ)
+    histogram(y; bins=range)
+    ŷ = model(x')
+    histogram!(ŷ'; bins=range)
+end
