0d3dc6e5de0d7980600c6375a9cf7fef6f897cb4
josemanuel22 <josemanuel.defrutos22@gmail.com>
2023-08-04 16:01:49 +0200

    type_stability

--- examples/CostFunction.jl
+++ examples/CostFunction.jl
@@ -7,20 +7,21 @@ using Plots
 Compute the cost function of a model with respect to a target function.
 """
 function proxi_cost_function(
-    mesh::Vector{LinRange{Float64, Int64}},
+    mesh::Vector{LinRange{Float64,Int64}},
     model::Function,
     target::Function,
     K::Int,
-    n_samples::Int)::Vector{Float64}
-
-    μ₁::Float64 = 0.; σ₁::Float64 = 1.
+    n_samples::Int,
+)::Vector{Float64}
+    μ₁::Float64 = 0.0
+    σ₁::Float64 = 1.0
     losses::Vector{Float64} = []
     ms, bs = mesh
 
     for mᵢ in ms
         for bᵢ in bs
-            loss = 0.
-            aₖ = zeros(K+1)
+            loss = 0.0
+            aₖ = zeros(K + 1)
             aₖ = ThreadsX.sum(1:n_samples) do _
                 x = rand(Normal(μ₁, σ₁), K)
                 yₖ = model.(x', m=mᵢ, b=bᵢ)
@@ -40,10 +41,12 @@ end;
 Compute the cost function of a model with respect to a target function.
 """
 function real_cost_function(
-    mesh::Vector{LinRange{Float64, Int64}},
-    model::Function, target::Function,
-    K::Int, n_samples::Int)::Vector{Float64}
-
+    mesh::Vector{LinRange{Float64,Int64}},
+    model::Function,
+    target::Function,
+    K::Int,
+    n_samples::Int,
+)::Vector{Float64}
     losses::Vector{Float64} = []
     ms, bs = mesh
     for mᵢ in ms
@@ -60,24 +63,35 @@ end;
 
 if abspath(PROGRAM_FILE) == @__FILE__
     #model to learn
-    model1(x; m, b) = m * x + b
-    m = 3; b = 5
-    truthh(x) =  model1(x; m=m, b=b)
+    model(x; m, b) = m * x + b
+    m = 3
+    b = 5
+    real_model(x) = model(x; m=m, b=b)
 
     #Generating Traning Set
     μ = 0
     stddev = 1
 
-    res = 100; n_samples = 1000; K = 2;
-    losses = []
-    ms = LinRange(m-0, m+0, 1)
-    bs = LinRange(b-50, b+50, res)
+    res = 100
+    n_samples = 1000
+    K = 2
+    ms = LinRange(m - 50, m + 50, res)
+    bs = LinRange(b - 50, b + 50, res)
 
-    losses_proxi = proxi_cost_function([ms, bs], model1, truthh, K, n_samples)
-    losses_real = real_cost_function([ms, bs], model1, truthh, K, n_samples)
+    losses_proxi = proxi_cost_function([ms, bs], model, real_model, K, n_samples)
+    losses_real = real_cost_function([ms, bs], model, real_model, K, n_samples)
 
-    plot(bs, losses_proxi)
-    Plots.plot(ms, bs, reshape(losses_proxi, (res,res)),st=:surface, title=string("N=",n_samples," K=", K," res=",res))
+    #Plot the cost function longitudinal slice.
+    #plot(bs, losses_proxi)
+
+    #Plot the cost function surface.
+    plot(
+        ms,
+        bs,
+        reshape(losses_proxi, (res, res));
+        st=:surface,
+        title=string("N=", n_samples, " K=", K, " res=", res),
+    )
     xlabel!("m")
     ylabel!("b")
 end;
