94e1cc58f5d162ec3e6210d8f8d40a427366004d
josemanuel22 <josemanuel.defrutos22@gmail.com>
2023-08-22 21:22:36 +0200

    adding doc

---
+++ docs/make.jl
@@ -0,0 +1,17 @@
+using Documenter
+using AdaptativeBlockLearning
+
+println("Generating Docs")
+
+makedocs(
+    sitename = "AdaptativeBlockLearning",
+    format = Documenter.HTML(),
+    modules = [AdaptativeBlockLearning]
+)
+
+# Documenter can also automatically deploy documentation to gh-pages.
+# See "Hosting Documentation" and deploydocs() in the Documenter manual
+# for more information.
+#=deploydocs(
+    repo = "<repository url>"
+)=#
