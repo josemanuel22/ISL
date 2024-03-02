dab46df8a03f05ca950021693111e3b54a86abc4
josemanuel22 <josemanuel.defrutos22@gmail.com>
2024-03-02 10:51:07 +0100

    change readme

--- docs/make.jl
+++ docs/make.jl
@@ -4,23 +4,24 @@ using ISL
 println("Generating Docs")
 
 makedocs(;
-    sitename = "ISL",
-    format = Documenter.HTML(),
-    modules = [ISL],
+    sitename="ISL",
+    format=Documenter.HTML(),
+    modules=[ISL],
     pages=[
         "Home" => "index.md",
         "GAN" => "gan.md",
         "Example" => "example.md",
         "Benchmark" => "benchmark.md",
     ],
+    strict=false,
 )
 
 # Documenter can also automatically deploy documentation to gh-pages.
 # See "Hosting Documentation" and deploydocs() in the Documenter manual
 # for more information.
-deploydocs(
-    repo = "github.com/josemanuel22/ISL.git",
+deploydocs(;
+    repo="github.com/josemanuel22/ISL.git",
     target="build",
-    push_preview = true,
+    push_preview=true,
     devbranch="main",
 )
